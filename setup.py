f61219e7ca7c39ba2df0c9a793c22173c602748a
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-06-26 10:41:37 -0500

    update pip package to 1.0.2 and changes
    -add delete_all_enrollments_for_user() function and change setup.py packages argument to use setuptools.find_packages() to automatically find and install requests package without having to the user install it manually

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+import setuptools
 from distutils.core import setup
+
 setup(
  name = 'voiceit2',
- packages = ['voiceit2'], # this must be the same as the name above
- version = '1.0.1',
+ version = '1.0.2',
  description = 'VoiceIt API 2.0 Python Wrapper',
  author = 'Hassan Ismaeel',
  author_email = 'hassan@voiceit.io',
+ packages=setuptools.find_packages(),
  url = 'https://github.com/voiceittech/voiceit2-python', # use the URL to the github repo
- download_url = 'https://github.com/voiceittech/voiceit2-python/archive/1.0.1.tar.gz', # I'll explain this in a second
+ download_url = 'https://github.com/voiceittech/voiceit2-python/archive/1.0.2.tar.gz', # I'll explain this in a second
  keywords = ['biometrics', 'voice verification', 'voice biometrics'], # arbitrary keywords
  classifiers = [
  "Programming Language :: Python :: 3",
