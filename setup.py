51c1dbe2796a3f2708a5b7fd1e76fee50fb1cec8
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-06-26 11:03:30 -0500

    added install_requires argument to setup command

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from distutils.core import setup
 
 setup(
  name = 'voiceit2',
- version = '1.0.2',
+ version = '1.0.3',
  description = 'VoiceIt API 2.0 Python Wrapper',
  author = 'Hassan Ismaeel',
  author_email = 'hassan@voiceit.io',
  packages=setuptools.find_packages(),
+ install_requires=[
+     'requests',
+ ],
  url = 'https://github.com/voiceittech/voiceit2-python', # use the URL to the github repo
- download_url = 'https://github.com/voiceittech/voiceit2-python/archive/1.0.2.tar.gz', # I'll explain this in a second
+ download_url = 'https://github.com/voiceittech/voiceit2-python/archive/1.0.3.tar.gz', # I'll explain this in a second
  keywords = ['biometrics', 'voice verification', 'voice biometrics'], # arbitrary keywords
  classifiers = [
  "Programming Language :: Python :: 3",
