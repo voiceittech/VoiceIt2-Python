28de0b5aa900903c428de0a04d943be4de1ea2f9
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-09 15:21:46 -0500

    force test failure

--- tests.py
+++ tests.py
@@ -294,13 +294,13 @@ class TestVoiceIt2(unittest.TestCase):
 
         #  Test Face Enrollments
         ret = my_voiceit.create_face_enrollment(user_id, './https://s3.amazonaws.com/voiceit-api2-testing-files/test-data/faceEnrollmentArmaan1.mp4')
-        self.assertEqual(200, ret['status'])
+        self.assertEqual(201, ret['status'])
         self.assertEqual('SUCC', ret['responseCode'])
         ret = my_voiceit.create_face_enrollment(user_id, 'https://s3.amazonaws.com/voiceit-api2-testing-files/test-data/faceEnrollmentArmaan2.mp4')
-        self.assertEqual(200, ret['status'])
+        self.assertEqual(201, ret['status'])
         self.assertEqual('SUCC', ret['responseCode'])
         ret = my_voiceit.create_face_enrollment(user_id, 'https://s3.amazonaws.com/voiceit-api2-testing-files/test-data/faceEnrollmentArmaan3.mp4')
-        self.assertEqual(200, ret['status'])
+        self.assertEqual(201, ret['status'])
         self.assertEqual('SUCC', ret['responseCode'])
 
         #  Test Face Verification
