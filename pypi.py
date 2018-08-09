081749f0c4796ed502b7332fba388b56311c9f67
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-09 14:00:27 -0500

    update tests

--- pypi.py
+++ pypi.py
@@ -10,16 +10,20 @@ options, _ = search_command.parse_args([pkg_name])
 pypi_hits = search_command.search(pkg_name, options)
 hits = commands.search.transform_hits(pypi_hits)
 
+
+#  assign the verison number to the variable original_version
 for hit in hits:
     if hit['name'] == pkg_name:
         original_version = hit['versions'][0]
 
+#  convert the original_version string into an array (3 items that are separated by a '.')
 version_split =  original_version.split('.')
 #  Increment the minor version by 1, and reassign it to the last version array
 version_split[2] =  str(int(version_split[2]) + 1)
 #  Concatenate the array into a single string with each section separated by '.' (just like we started out with)
 new_version = '.'.join(version_split)
 
+#  Produce the string to store in ./setup.py
 setup_string = '''import setuptools
 from distutils.core import setup
 
@@ -34,7 +38,7 @@ setup(
      "requests",
  ],
  url = "https://github.com/voiceittech/voiceit2-python",
- download_url = "https://github.com/voiceittech/voiceit2-python/archive/1.0.5.tar.gz",
+ download_url = "https://github.com/voiceittech/voiceit2-python/archive/''' + new_version + '''.tar.gz",
  keywords = ["biometrics", "voice verification", "voice biometrics"],
  classifiers = [
  "Programming Language :: Python :: 3",
@@ -42,6 +46,7 @@ setup(
  "Operating System :: OS Independent"],
 )'''
 
+#  store string into ./setup.py
 with open('./setup.py', 'w') as setup:
     setup.write(setup_string)
 
@@ -59,7 +64,6 @@ except  requests.exceptions.HTTPError as e:
     exit(1)
 
 #  Update PyPi with newest package
-
 pypiusername = os.environ['PYPIUSERNAME']
 pypipassword = os.environ['PYPIPASSWORD']
 
