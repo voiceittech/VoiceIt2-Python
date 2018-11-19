fcf0951627d560b083c4db8dd002cc629da8de6f
Andrew Lee <me@gilgameshskytrooper.io>
2018-11-19 11:55:01 -0600

    add autoemail functionality

---
+++ email.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Just the git commit message title
+title=$(git log -1 --pretty=%B | head -n 1)
+git checkout master
+# Save the messages into an array called message
+IFS=$'\n' message=($(git log -1 --pretty=%B | sed -e '1,2d'))
+
+echo "which sed"
+which sed
+echo "which curl"
+which curl
+
+echo $title
+for i in "${message[@]}"
+do
+  echo $i
+done
+
+if [[ $title = *"SENDEMAIL"* ]];
+then
+  formattedmessages=''
+  for i in "${message[@]}"
+  do
+    formattedmessages=$formattedmessages'|'$i
+  done
+
+  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'"}'
+  curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/28"
+fi
