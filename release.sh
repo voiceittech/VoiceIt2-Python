962eece67dcadbdaff5c95ba27ffaccb05f90a72
Andrew Lee <me@gilgameshskytrooper.io>
2019-03-14 12:18:05 -0500

    update release.sh

--- release.sh
+++ release.sh
@@ -106,7 +106,7 @@ then
         formattedmessages=$formattedmessages'|'$i
       done
 
-      json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "pip install --upgrade voiceit2</code></div><br />"}'
+      json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "pip3 install --upgrade voiceit2</code></div><br />"}'
       curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/28"
     fi
     exit 0
