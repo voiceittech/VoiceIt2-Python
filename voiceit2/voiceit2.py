848302769543ccef836e465d6be685884628c21e
gilgameshskytrooper <me@gilgameshskytrooper.io>
2021-07-19 16:54:12 -0500

    DEVOPS-101/DEVOPS-104 (RELEASEPATCH)

--- voiceit2/voiceit2.py
+++ voiceit2/voiceit2.py
@@ -4,7 +4,7 @@ import urllib
 
 class VoiceIt2:
     base_URL = 'https://api.voiceit.io'
-    version = '2.6.0'
+    version = '2.6.1'
     voiceit_basic_auth_credentials = ''
     notification_url = ''
 
